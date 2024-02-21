3219af04351531a15ea56967c97e888c7a87cd06
Glenda <107262765+glendalvesx@users.noreply.github.com>
2024-02-21 14:40:19 +0000

    Endpoint [GET] /item/:id criado

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 // Read All -> [GET] /item
 app.get('/item', function (req, res) {
-  res.send(lista)
+ // Envio a lista inteira como resposta HTTP  res.send(lista)
 })
+
+// Read By ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res){
+  //Acesso o ID no parâmetro de rota
+  const id = req.params.id
+
+  //Acesso item na lista baseado no ID recebido
+  const item = lista[id]
+
+  // Envio o item obtido como resposta HTTP
+  res.send(item)
+})
+
+
 
 app.listen(3000)
